f2f2029e2eb2bca14e4550448df5a0fea0ad970d
Nimrod Huaira <nimax29@gmail.com>
2024-06-11 23:34:19 -0600

    template

---
+++ dist/index.579125c3.js
@@ -0,0 +1,18 @@
+/* NOTE: The instructions are in index.html */ /* --- STEP (2) ---
+ * - Make this function take an array as a parameter.
+ * - Return the count of items in the array that are multiples of three.
+ * - Keep this function stand-alone. I.e. it should not know ANYTING about DOM.
+ */ function countThreevens() {}
+/* --- STEP (5) ---
+ * - Add a click handler on the button.
+ * - Read the numbers from the input and turn them into an array.
+ * - Call countThreevens with the array.
+ * - Replace "blah" (and only blah) with the result.
+ */ function handleClick() {}
+/* TROUBLESHOOTING TIP:
+ * Because of how the sandbox is set up, this script creates NO global
+ * variables accessible in the HTML file. To make a value from this file
+ * visible in the HTML file, you have to store it on window.
+ */ window.handleClick = handleClick;
+
+//# sourceMappingURL=index.579125c3.js.map
--- readme.md
+++
@@ -1,8 +0,0 @@
-## JS projects
-
-Repository with small projects on JavaScript.
-
-
-### Author
-
-Nim Huaira
